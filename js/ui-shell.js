ed17a674bb7625ffe7d57e139c0254a11aa442ca
alessandrogabe <147330946+alessandrogabe@users.noreply.github.com>
2026-08-14 09:02:09 +0200

    feat: load AUX repair TX sidecar

--- js/ui-shell.js
+++ js/ui-shell.js
@@ -7,6 +7,7 @@ import './tx-optical-view.js';
 import './tx-flow-ui.js';
 import './rx-performance-policy.js';
 import './tx-profile-policy.js';
+import './tx-aux-repair.js';
 
 const homeView = document.getElementById('homeView');
 const workspaceView = document.getElementById('workspaceView');
